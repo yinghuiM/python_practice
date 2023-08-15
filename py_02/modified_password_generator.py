6bd6b3b285955aa0581784b42828677091012cdb
yinghuiM <yinghui_meng@gotask.co.jp>
2023-08-15 18:43:00 +0900

    fix

--- py_02/modified_password_generator.py
+++ py_02/modified_password_generator.py
@@ -30,26 +30,23 @@ def random_password(length, include_uppercase, include_symbol):
         "".join(symbol) if include_symbol else ''
     ]
 
-    if include_uppercase and include_symbol:
-        weights = [0.4, 0.3, 0.2, 0.1]
-    elif include_uppercase:
-        weights = [0.5, 0.3, 0.2, 0]
-    elif include_symbol:
-        weights = [0.5, 0.3, 0, 0.2]
-    else:
-        weights = [0.6, 0.4]
+    weights = [0.4, 0.3, 0.2, 0.1]
 
     for char_set, weight in zip(character_sets, weights):
+        if (char_set == ""):
+            continue
         result += "".join(random.choices(char_set,
                           k=math.floor(length * weight)))
     return result
 
 
+print(random_password(3, False, False))
+
 if __name__ == "__main__":
     start_time = time.time()
     unique_passwords = set()
 
-    while time.time() - start_time < 5:
+    while time.time() - start_time < 1:
         password = random_password(16, True, True)
         if password in unique_passwords:
             print("The password has been generated:", password)
